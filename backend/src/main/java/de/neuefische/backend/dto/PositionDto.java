77bb05f5c31e471d86fe3c76244ef2690f636e02
Marlin Noethig <marlin3003@gmx.de>
2022-05-19 13:04:10 +0200

    added post endpoint to positions and wrote corresponding tests

---
+++ backend/src/main/java/de/neuefische/backend/dto/PositionDto.java
@@ -0,0 +1,20 @@
+package de.neuefische.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PositionDto {
+    private String name;
+    private String description;
+    private double price;
+    private double amount;
+    private float tax;
+}
