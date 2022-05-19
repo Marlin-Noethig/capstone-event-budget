9bde6bdd75654a44431840193a5c0fee4220b374
Marlin Noethig <marlin3003@gmx.de>
2022-05-19 09:56:58 +0200

    added AppTitle to App and styled a little

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import {Route, Routes} from "react-router-dom";
 import BudgetOverview from "./pages/BudgetOverview";
+import AppTitle from "./components/AppTitle";
 
 function App() {
 
 
     return (
         <div className="App">
+            <AppTitle/>
             <Routes>
                 <Route path="/"
                        element={<BudgetOverview/>}
---
+++ frontend/src/components/AppTitle.tsx
@@ -0,0 +1,7 @@
+import "./styles/AppTitle.css"
+
+export default function AppTitle(){
+    return(
+        <div className={"app-title"}>EventBudget</div>
+    )
+}
