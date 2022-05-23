db39ff8d466f4201e0fb1e998d280a39d341be14
Marlin Noethig <marlin3003@gmx.de>
2022-05-23 17:15:30 +0200

    refactored custom hook call of usePositions to one element below

--- frontend/src/pages/BudgetOverview.tsx
+++ frontend/src/pages/BudgetOverview.tsx
@@ -1,19 +1,12 @@
-import usePositions from "../hooks/usePositions";
 import PositionList from "../components/budget-overview/PositionList";
 import EventDetailView from "../components/budget-overview/EventDetailView";
 
 export default function BudgetOverview(){
 
-    const {positions, addNewPosition, updatePositionById, removePositionById} = usePositions();
-
     return(
         <div className={"budget-overview-container"}>
             <EventDetailView/>
-            <PositionList positions={positions}
-                          updatePosition={updatePositionById}
-                          deletePosition={removePositionById}
-                          addNewPosition={addNewPosition}
-            />
+            <PositionList/>
         </div>
     )
 }
