9ace27da81ca7187c8fa4c4158555640a61951b8
Marlin Noethig <marlin3003@gmx.de>
2022-05-25 11:42:01 +0200

    created basic backend Structure for AppUser handling

---
+++ backend/src/main/java/de/neuefische/backend/security/model/AppUser.java
@@ -0,0 +1,21 @@
+package de.neuefische.backend.security.model;
+
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Builder
+@With
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "appusers")
+public class AppUser {
+
+    @Id
+    private String id;
+    private String mail;
+    private String password;
+
+}
---
+++ backend/src/main/java/de/neuefische/backend/security/service/AppUserDetailsService.java
@@ -0,0 +1,27 @@
+package de.neuefische.backend.security.service;
+
+import de.neuefische.backend.security.model.AppUser;
+import de.neuefische.backend.security.repository.AppUserRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AppUserDetailsService implements UserDetailsService{
+    private final AppUserRepository appUserRepository;
+
+    public AppUserDetailsService(AppUserRepository appUserRepository) {
+        this.appUserRepository = appUserRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String mail) throws UsernameNotFoundException {
+        AppUser appUser = appUserRepository.findByMail(mail)
+                .orElseThrow(() -> new UsernameNotFoundException("No user with this mail registered"));
+        return new User(appUser.getMail(), appUser.getPassword(), List.of());
+    }
+}
