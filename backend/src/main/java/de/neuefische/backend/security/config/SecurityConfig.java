174e1050f8c8fb5286521904b139b23ffc9cda7f
Marlin Noethig <marlin3003@gmx.de>
2022-05-25 13:16:06 +0200

    completed backend-logic for user authentication

---
+++ backend/src/main/java/de/neuefische/backend/security/config/SecurityConfig.java
@@ -0,0 +1,54 @@
+package de.neuefische.backend.security.config;
+
+import de.neuefische.backend.security.filter.JwtAuthFilter;
+import de.neuefische.backend.security.service.AppUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final AppUserDetailsService appUserDetailsService;
+    private final JwtAuthFilter jwtAuthFilter;
+
+    @Autowired
+    public SecurityConfig(AppUserDetailsService appUserDetailsService, JwtAuthFilter jwtAuthFilter) {
+        this.appUserDetailsService = appUserDetailsService;
+        this.jwtAuthFilter = jwtAuthFilter;
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(appUserDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new Argon2PasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/api/**").authenticated()
+                .antMatchers("/auth/login").permitAll()
+                .and().addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+}
---
+++ backend/src/main/java/de/neuefische/backend/security/controller/AppUserAuthController.java
@@ -0,0 +1,32 @@
+package de.neuefische.backend.security.controller;
+
+import de.neuefische.backend.security.model.AppUser;
+import de.neuefische.backend.security.service.JwtUtilService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AppUserAuthController {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtilService jwtUtilService;
+
+    @Autowired
+    public AppUserAuthController(AuthenticationManager authenticationManager, JwtUtilService jwtUtilService) {
+        this.authenticationManager = authenticationManager;
+        this.jwtUtilService = jwtUtilService;
+    }
+
+    @PostMapping("/login")
+    public  String login(@RequestBody AppUser appUser) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(appUser.getMail(), appUser.getPassword()));
+        return jwtUtilService.creatToken(appUser.getMail());
+    }
+
+}
