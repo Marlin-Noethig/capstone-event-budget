3a490b68ee15d3f0dc052960a260b37e5c3c5392
Marlin Nöthig <73184960+Marlin-Noethig@users.noreply.github.com>
2022-05-30 22:07:35 +0200

    Apply suggestions from code review

--- frontend/src/routing/RequireAuth.tsx
+++ frontend/src/routing/RequireAuth.tsx
@@ -2,8 +2,6 @@ import {useContext} from "react";
 import {AuthContext} from "../context/AuthProvider";
 import {Navigate, Outlet} from "react-router-dom";
 
-
-
 export default function RequireAuth() {
     const {token} = useContext(AuthContext);
 
