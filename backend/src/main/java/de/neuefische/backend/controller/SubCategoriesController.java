3e7c9d3ca9505f1c6782ff86f284dfeb8b91dd9d
Marlin Noethig <marlin3003@gmx.de>
2022-06-01 17:21:12 +0200

    fixed faulty urls which caused 404s

--- backend/src/main/java/de/neuefische/backend/controller/SubCategoriesController.java
+++ backend/src/main/java/de/neuefische/backend/controller/SubCategoriesController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/sub-categories")
+@RequestMapping("/api/sub-categories/")
 public class SubCategoriesController {
 
     private final SubCategoriesService subCategoriesService;
