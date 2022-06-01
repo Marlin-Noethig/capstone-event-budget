e140b6a7b0197cec39b83d9972b653e5616fded6
Marlin Nöthig <73184960+Marlin-Noethig@users.noreply.github.com>
2022-06-01 19:08:36 +0200

    Apply suggestions from code review

--- backend/src/main/java/de/neuefische/backend/security/filter/JwtAuthFilter.java
+++ backend/src/main/java/de/neuefische/backend/security/filter/JwtAuthFilter.java
@@ -34,8 +34,6 @@ public class JwtAuthFilter extends OncePerRequestFilter {
         this.appUserRepository = appUserRepository;
     }
 
-
-
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String token = getAuthToken(request);
