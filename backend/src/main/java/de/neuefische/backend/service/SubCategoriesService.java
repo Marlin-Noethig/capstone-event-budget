d91a012887705b28a5c0a3d6ac37f35f163c7a0e
Marlin Noethig <marlin3003@gmx.de>
2022-06-01 16:46:33 +0200

    added preauthorization in methods of SubCategoriesService

--- backend/src/main/java/de/neuefische/backend/service/SubCategoriesService.java
+++ backend/src/main/java/de/neuefische/backend/service/SubCategoriesService.java
@@ -5,6 +5,7 @@ import de.neuefische.backend.model.MainCategory;
 import de.neuefische.backend.model.SubCategory;
 import de.neuefische.backend.repository.MainCategoriesRepo;
 import de.neuefische.backend.repository.SubCategoriesRepo;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -21,10 +22,12 @@ public class SubCategoriesService {
         this.mainCategoriesRepo = mainCategoriesRepo;
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     public List<SubCategory> getSubCategories(){
         return subCategoriesRepo.findAll();
     }
 
+    @PreAuthorize("hasAuthority('USER')")
     public List<SubCategory> getSubCategoriesByUserId(String idOfCurrentUser) {
         List<MainCategory> mainCategories = mainCategoriesRepo.findAllByUserIdsContaining(idOfCurrentUser);
         ArrayList<String> mainCategoryIds = new ArrayList<>();
