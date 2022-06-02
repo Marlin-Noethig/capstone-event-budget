60b4f91b4f5ddc088c60397e26ad2ec9d6392d24
Marlin Noethig <marlin3003@gmx.de>
2022-06-02 16:17:45 +0200

    implemented backend architecture for Event data and get endpoint

---
+++ backend/src/main/java/de/neuefische/backend/controller/EventsController.java
@@ -0,0 +1,34 @@
+package de.neuefische.backend.controller;
+
+import de.neuefische.backend.model.Event;
+import de.neuefische.backend.security.service.utils.AuthUtils;
+import de.neuefische.backend.service.EventsService;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/events/")
+public class EventsController {
+
+    private final EventsService eventsService;
+
+    public EventsController(EventsService eventsService) {
+        this.eventsService = eventsService;
+    }
+
+    @GetMapping
+    public List<Event> getEvents(Authentication authentication) {
+        String roleOfCurrentUser = authentication.getAuthorities().toArray()[0].toString();
+
+        if (roleOfCurrentUser.equals("ADMIN")) {
+            return eventsService.getEvents();
+        } else {
+            String idOfCurrentUser = AuthUtils.getIdOfCurrentUser(authentication);
+            return eventsService.getEventsByUserId(idOfCurrentUser);
+        }
+    }
+}
