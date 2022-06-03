1b5336012670a791497afb2116fbdd302f08519e
Marlin Noethig <marlin3003@gmx.de>
2022-06-03 14:28:44 +0200

    Display events as card with main categories and corresponding data

--- frontend/src/components/event-overview/EventCard.tsx
+++ frontend/src/components/event-overview/EventCard.tsx
@@ -2,6 +2,12 @@ import {MainCategory} from "../../model/MainCategory";
 import {SubCategory} from "../../model/SubCategory";
 import {Position} from "../../model/Position";
 import {EventData} from "../../model/EventData";
+import BalanceView from "../budget-overview/BalanceView";
+import {getBalance} from "../../service/utils/sumHelpers";
+import {useContext} from "react";
+import {AuthContext} from "../../context/AuthProvider";
+import MainCategoryOverview from "./MainCategoryOverview";
+import "./styles/EventCard.css"
 
 export type EventCardProps = {
     event: EventData;
@@ -10,10 +16,19 @@ export type EventCardProps = {
     positions: Position[],
 }
 
-export default function EventCard({event, mainCategories, subCategories, positions}: EventCardProps){
-    return(
+export default function EventCard({event, mainCategories, subCategories, positions}: EventCardProps) {
+
+    const positionsOfEvent = positions.filter(position => position.eventId === event.id);
+    const {showBalance} = useContext(AuthContext)
+
+    return (
         <div className={"event-card-container"}>
-            <div className={"event-heading"}>{event.name}</div>
+            <div className={"event-card-heading"}>{event.name}</div>
+            {mainCategories.map(mainCategory => <MainCategoryOverview mainCategory={mainCategory}
+                                                                      subCategories={subCategories}
+                                                                      positions={positionsOfEvent}/>
+            )}
+            {showBalance && <BalanceView sum={getBalance(positionsOfEvent, subCategories, mainCategories)}/>}
         </div>
     )
 }
---
+++ frontend/src/components/event-overview/MainCategoryOverview.tsx
@@ -0,0 +1,30 @@
+import {MainCategory} from "../../model/MainCategory";
+import {SubCategory} from "../../model/SubCategory";
+import {Position} from "../../model/Position";
+import {formatMoney} from "../../service/utils/beatifyHelpers";
+import {getMainSum} from "../../service/utils/sumHelpers";
+
+
+type MainCategoryOverviewProps = {
+    mainCategory: MainCategory,
+    subCategories: SubCategory[],
+    positions: Position[],
+}
+
+export default function MainCategoryOverview({mainCategory, subCategories, positions}:MainCategoryOverviewProps){
+    const filteredSubCategories = subCategories.filter(subCategory => subCategory.mainCategoryId === mainCategory.id)
+    const isIncomeClassName = mainCategory.income ? "incomes" : "expenses"
+
+    return(
+        <div>
+            <div className={"category-view main-category " + isIncomeClassName}>
+                <span className={"main-category-name"}>
+                    {mainCategory.name}
+                </span>
+                <span className={"main-sum"}>
+                    {formatMoney(getMainSum(positions, filteredSubCategories))} €
+                </span>
+            </div>
+        </div>
+    )
+}
