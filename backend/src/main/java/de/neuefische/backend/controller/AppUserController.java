d368cb3785221c56ae0aef1a2455cfe11c42cb51
Marlin Noethig <marlin3003@gmx.de>
2022-06-09 13:14:56 +0200

    implemented GET endpoint for List of all users

--- backend/src/main/java/de/neuefische/backend/controller/AppUserController.java
+++ backend/src/main/java/de/neuefische/backend/controller/AppUserController.java
@@ -1,16 +1,33 @@
 package de.neuefische.backend.controller;
 
 import de.neuefische.backend.security.dto.AppUserInfoDto;
+import de.neuefische.backend.service.AppUsersService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/user/")
 public class AppUserController {
+
+    private final AppUsersService appUsersService;
+
+    @Autowired
+    public AppUserController(AppUsersService appUsersService) {
+        this.appUsersService = appUsersService;
+    }
+
     @GetMapping("current")
     public AppUserInfoDto getCurrentUser(Authentication authentication) {
         return (AppUserInfoDto) authentication.getPrincipal();
+    }
+
+    @GetMapping
+    public List<AppUserInfoDto> getAllUsers(){
+        return appUsersService.getAllUserInfos();
     }
 }
