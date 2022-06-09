ec46a28e67a93822de9c57a9cdefbf21fabeabd4
Marlin Noethig <marlin3003@gmx.de>
2022-06-09 16:17:54 +0200

    added Tests for get all users endpoint

--- backend/src/test/java/de/neuefische/backend/controller/AppUserControllerTest.java
+++ backend/src/test/java/de/neuefische/backend/controller/AppUserControllerTest.java
@@ -12,13 +12,17 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment =  SpringBootTest.WebEnvironment.RANDOM_PORT)
 class AppUserControllerTest {
 
     private String userJwt1;
+    private String adminJwt1;
     final String userMail1 = "max@muster.de";
+    final String adminMail1 = "mimi@muster.de";
 
     @LocalServerPort
     private int port;
@@ -37,6 +41,7 @@ class AppUserControllerTest {
         appUserRepository.deleteAll();
 
         userJwt1 = generateJwtAndSaveUserToRepo("u1", userMail1, "Max", "Muster", "Supercompany", "USER");
+        adminJwt1 =generateJwtAndSaveUserToRepo("a1", adminMail1, "Mimi", "Muster", "Supercompany", "ADMIN");
     }
 
     @Test
@@ -62,6 +67,51 @@ class AppUserControllerTest {
 
         assertEquals(expected, actual);
 
+    }
+
+    @Test
+    void getInfosOfAllUsers_whenAdmin_shouldReturnListOfAllUsers(){
+        //WHEN
+        List<AppUserInfoDto> actual = webTestClient.get()
+                .uri("http://localhost:" + port + "/api/user/")
+                .headers(httpHeaders -> httpHeaders.setBearerAuth(adminJwt1))
+                .exchange()
+                .expectStatus().is2xxSuccessful()
+                .expectBodyList(AppUserInfoDto.class)
+                .returnResult()
+                .getResponseBody();
+
+        //THEN
+        AppUserInfoDto expectedUser1 = AppUserInfoDto.builder()
+                .id("u1")
+                .mail(userMail1)
+                .firstName("Max")
+                .lastName("Muster")
+                .company("Supercompany")
+                .role("USER").build();
+
+        AppUserInfoDto expectedUser2 = AppUserInfoDto.builder()
+                .id("a1")
+                .mail(adminMail1)
+                .firstName("Mimi")
+                .lastName("Muster")
+                .company("Supercompany")
+                .role("ADMIN").build();
+
+        List<AppUserInfoDto> expected = List.of(expectedUser1, expectedUser2);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void getInfosOfAllUsers_whenUser_shouldReturnError403isForbidden(){
+        //WHEN
+        webTestClient.get()
+                .uri("http://localhost:" + port + "/api/user/")
+                .headers(httpHeaders -> httpHeaders.setBearerAuth(userJwt1))
+                .exchange()
+                //THEN
+                .expectStatus().isForbidden();
     }
 
     private String generateJwtAndSaveUserToRepo(String id, String mail, String firstName, String lastName, String company, String role) {
