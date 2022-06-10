d42ce7870503f56d166e7dc06d5a5211bb01d519
Marlin Noethig <marlin3003@gmx.de>
2022-06-10 18:32:01 +0200

    implemented api-handling for sub-categories

--- frontend/src/hooks/useSubCategories.ts
+++ frontend/src/hooks/useSubCategories.ts
@@ -1,8 +1,14 @@
 import {useContext, useEffect, useState} from "react";
-import {getSubCategories} from "../service/api-service/sub-categories-api-service";
+import {
+    deleteSubCategoryById,
+    getSubCategories,
+    postSubCategory,
+    putSubCategoryById
+} from "../service/api-service/sub-categories-api-service";
 import {SubCategory} from "../model/SubCategory";
 import {AuthContext} from "../context/AuthProvider";
 import {handleRequestError} from "../service/utils/errorHandlers";
+import {toast} from "react-toastify";
 
 export default function useSubCategories(){
     const [subCategories, setSubCategories] = useState<SubCategory[]>([]);
@@ -14,5 +20,26 @@ export default function useSubCategories(){
             .catch((error) => handleRequestError(error.response.status));
     }, [token])
 
-    return {subCategories}
+    const addSubCategory = (newSubCategory: Omit<SubCategory, "id">) =>{
+        postSubCategory(newSubCategory, token)
+            .then(addedSubCategory => setSubCategories([...subCategories, addedSubCategory]))
+            .then(() => toast.success(`${newSubCategory.name} has been added.`))
+            .catch((error) => handleRequestError(error.response.status));
+    }
+
+    const updateSubCategoryById = (id: string, subCategoryToUpdate: Omit<SubCategory, "id">) => {
+        putSubCategoryById(id, subCategoryToUpdate, token)
+            .then(updatedSubCategory => setSubCategories(subCategories.map(subCategory => subCategory.id === id ? updatedSubCategory : subCategory)))
+            .then(() => toast.success(`${subCategoryToUpdate.name} has been updated.`))
+            .catch((error) => handleRequestError(error.response.status));
+    }
+
+    const removeSubCategoryById = (id: string, name: string) => {
+        deleteSubCategoryById(id, token)
+            .then(() => setSubCategories(subCategories.filter(subCategory => subCategory.id !== id)))
+            .then(() => toast.success(`${name} has been deleted.`))
+            .catch((error) => handleRequestError(error.response.status));
+    }
+
+    return {subCategories, addSubCategory, updateSubCategoryById, removeSubCategoryById}
 }
