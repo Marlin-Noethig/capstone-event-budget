c62ce278cfc776b32151b1f082cfb1d7f877034d
Marlin Noethig <marlin3003@gmx.de>
2022-06-16 09:23:39 +0200

    laid basic navbar without styling

--- frontend/src/components/AppTitle.tsx
+++ frontend/src/components/AppTitle.tsx
@@ -1,7 +1,7 @@
 import "./styles/AppTitle.css"
 import {useContext} from "react";
 import {AuthContext} from "../context/AuthProvider";
-import {Link} from "react-router-dom";
+import NavigationBar from "./NavigationBar";
 
 
 export default function AppTitle() {
@@ -19,18 +19,13 @@ export default function AppTitle() {
 
     return (
         <div className={"app-title-container"}>
-            <div>
                 {currentUser ?
                     <div className={"display-current-user"}>{`Hey, ${currentUser.firstName} ${currentUser.lastName}!`} </div>
                     :
-                    <></>
+                    <div> </div>
                 }
-            </div>
             <div className={"app-title"}>EventBudget</div>
-            <div className={"logout-button-wrapper"}>
-                {token && isAdmin && <Link to={"/admin"}>Admin</Link>}
-                {token && <button className={"logout-button"} onClick={logout}>Logout</button>}
-            </div>
+            {token ? <NavigationBar isAdmin={isAdmin} logout={logout}/> : <div> </div>}
         </div>
     )
 }
---
+++ frontend/src/components/NavigationBar.tsx
@@ -0,0 +1,19 @@
+import {useNavigate} from "react-router-dom";
+
+
+type NavigationBarProps = {
+    isAdmin: boolean
+    logout: () => void
+}
+
+export default function NavigationBar({isAdmin, logout}:NavigationBarProps) {
+    const navigate = useNavigate()
+
+    return (
+        <nav>
+                <div className={"nav-element"} onClick={() => navigate("/")}>HOME</div>
+                {isAdmin && <div className={"nav-element"} onClick={() => navigate("/admin")}>ADMIN</div>}
+                <div className={"nav-element"} onClick={logout}>LOGOUT</div>
+        </nav>
+    )
+}
