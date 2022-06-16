5970462a4d8164a51053d806500939e21a7e6005
Marlin Noethig <marlin3003@gmx.de>
2022-06-16 22:46:39 +0200

    built in restore button for change log entries

--- frontend/src/components/budget-overview/PositionChangeLogEntry.tsx
+++ frontend/src/components/budget-overview/PositionChangeLogEntry.tsx
@@ -8,21 +8,24 @@ type PositionChangeLogEntryProps = {
 
 export default function PositionChangeLogEntry({change}:PositionChangeLogEntryProps){
     return <div className={"position-change-log-entry"}>
-        <div className={"change-metadata"}>
-            <div>Changed by: {change.userInfo}</div>
-            <div>Changed at: {new Date(change.timestamp).toLocaleString()}</div>
-            <div>Method: {change.method.toLowerCase()}</div>
-        </div>
-        <div className={"change-data"}>
-            <div className={"change-data-text"}>
-                <div className={"change-data-name"}>{change.data.name}</div>
-                <div>Description: {change.data.description}</div>
+        <div className={"position-change-log-entry-wrapper"}>
+            <div className={"change-metadata"}>
+                <div>Changed by: {change.userInfo}</div>
+                <div>Changed at: {new Date(change.timestamp).toLocaleString()}</div>
+                <div>Method: {change.method.toLowerCase()}</div>
             </div>
-            <div className={"change-data-numbers"}>
-                <div>Amount: {change.data.amount}</div>
-                <div>Tax: {change.data.tax} %</div>
-                <div>Net price: {formatMoney(change.data.price)} €</div>
+            <div className={"change-data"}>
+                <div className={"change-data-text"}>
+                    <div className={"change-data-name"}>{change.data.name}</div>
+                    <div>Description: {change.data.description}</div>
+                </div>
+                <div className={"change-data-numbers"}>
+                    <div>Amount: {change.data.amount}</div>
+                    <div>Tax: {change.data.tax} %</div>
+                    <div>Net price: {formatMoney(change.data.price)} €</div>
+                </div>
             </div>
         </div>
+        <div className={"restore-position"}>RESTORE</div>
     </div>
 }
